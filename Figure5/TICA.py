d55e9d521236b5d16a23ebab2ded77cca3c37791
MaxKim <136273348+KihongCode@users.noreply.github.com>
2024-04-20 15:58:37 -0500

    Update TICA.py

--- Figure5/TICA.py
+++ Figure5/TICA.py
@@ -49,19 +49,12 @@ tica_concatenated = np.concatenate(projection)
 l = free_energy_plot(tica_concatenated[:,0],tica_concatenated[:,1],weightsClassA = weights, vmax = 7)
 l[1].set_xlabel('time-lagged independent component 1',fontsize=20)
 l[1].set_ylabel('time-lagged independent component 2',fontsize=20)
-
-
-
 l[1].set_ylim(-3,11)
 yticks = range(-3,12,2)
 l[1].set_yticks(yticks)
 l[1].set_xlim(-4,4)
 xticks = range(-4,5,2)
 l[1].set_xticks(xticks)
-
-
-
-
 l[1].scatter(projection1[0],  projection1[1], marker='*', s=100, facecolors='k', edgecolors='r', label = 'Inactive')
 l[1].scatter(projection2[0],  projection2[1], marker='d', s=100, facecolors='k', edgecolors='r', label = 'Active')
 l[1].legend(fontsize=20,frameon=False,loc='upper left')
